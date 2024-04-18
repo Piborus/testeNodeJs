c679f6ef039cd3c5bb2fc6b5e6a7157dd551d2e1
Haroldo <haroldomorais92@gmail.com>
2024-04-18 11:29:33 -0300

    termino da aula 2

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+const somaHorasExtras = (salario, valorHorasExtras) =>
+  salario + valorHorasExtras;
 
-console.log(somaHorasExtras(2000, 500));
+const calculaDesconto = (salario, desconto) => salario - desconto;
+
+export { somaHorasExtras, calculaDesconto };
+
+/*const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        throw {};
+      }
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`✅ ${titulo}`);
+  } catch {
+    console.error(`❌ ${titulo}`);
+  }
+};
+
+teste('somaHorasExtras', (salario) => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+teste('calculaDesconto', () => {
+  const esperado = 2300;
+  const retornado = calculaDesconto(2500, 200);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});*/
